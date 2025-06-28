0847f9bb37d09da3a8a00a32f305a69bc3dae52f
Alexander Knopov <sknopov@gmail.com>
2025-06-28 18:01:59 -0400

    Enhanced UI - introduced "extract" button enabled when both source and destination are set

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from tkinter import filedialog
 
 source_file = ""
 destination_file = ""
-widget_width = 20
+widget_width = 30
 
 # Google: "python create GUI with file open dialog"
 
@@ -11,33 +11,43 @@ def start_app_window():
     global root
     global source_label
     global destination_label
+    global extract_button
+
     root = tk.Tk()
     root.title("Soil Test Data Extractor")
-    root.geometry("350x200")
+    root.geometry("500x200")
     root.protocol("WM_DELETE_WINDOW", on_closing)
 
+    frame = tk.Frame(root)    
     source_button = tk.Button(
-        root, text="Source File", width=widget_width, command=select_source_file
+        frame, text="Source File", width=widget_width, command=select_source_file
     )
     destination_button = tk.Button(
-        root,
+        frame,
         text="Destination File",
         width=widget_width,
         command=select_destination_file,
     )
-    # extract_button = tk.Button(
-    #     root,
-    #     text="Extract",
-    #     command=extract,
-    # )
-    source_label = tk.Label(root, borderwidth=1, width=widget_width, relief="groove")
-    destination_label = tk.Label(root, borderwidth=1, width=widget_width, relief="groove")
+    extract_button = tk.Button(
+        root,
+        text="Extract",
+        state="disabled",
+        padx=20,
+        command=extract,
+    )
+    source_label = tk.Label(
+        frame, borderwidth=1, width=widget_width, relief="groove"#, anchor="e"
+    )
+    destination_label = tk.Label(
+        frame, borderwidth=1, width=widget_width, relief="groove"#, anchor="e"
+    )
 
+    frame.pack(padx=10, pady=10)
     source_button.grid(row=0, column=0, padx=10, pady=10)
     destination_button.grid(row=0, column=1, padx=10, pady=10)
     source_label.grid(row=1, column=0, padx=10, pady=10)
     destination_label.grid(row=1, column=1, padx=10, pady=10)
-    # extract_button.pack(tk.CENTER, padx=10, pady=10)
+    extract_button.pack(padx=10, pady=10)
 
     root.mainloop()
 
@@ -53,7 +63,7 @@ def select_source_file():
     source_file = open_file_dialog()
     source_label.configure(text = source_file)
     source_label.update()
-
+    enable_extract()
 
 def select_destination_file():
     global destination_file
@@ -61,6 +71,7 @@ def select_destination_file():
     destination_file = open_file_dialog()
     destination_label.configure(text = destination_file)
     destination_label.update()
+    enable_extract()
 
 def extract():
     print("Starting extraction...")
@@ -75,5 +86,12 @@ def open_file_dialog():
         ],
     )
 
+def enable_extract():
+    global source_file
+    global destination_file
+    global extract_button
+    if source_file != "" and destination_file != "":
+        extract_button.config(state="normal")
+        extract_button.update()
 
 start_app_window()

61b2658c946140c878f8d526783e40106ad803e0
Alexander Knopov <sknopov@gmail.com>
2025-10-10 14:16:08 -0400

    Added pdf_extractor

--- pdf_extractor.py
+++ pdf_extractor.py
@@ -1,33 +1,120 @@
+import os
+import logging as log
+import math
+from typing import Any
 import fitz # PyMuPDF
+import converter as cnv
+import xcl_extractor as xcl
 
-class PdfExtractor:
-    def __init__(self, pdf_file: str):
-        self.doc = fitz.open(pdf_file)
+MAPPINGS = [
+    # PDF page, key string (lower case), num values, dest col, offsets (negative - absolute)
+    {"page": 0, "key": "client:", "num": 1, "out": "B", "offset": [0]},
+    {"page": 0, "key": "soil classification:", "num": 1, "out": "F", "offset": [0]},
+    {"page": 0, "key": "borehole/sample no.:", "num": 3, "out": "G", "offset": [0]},
+    {"page": 0, "key": "sample depth (m):", "num": 3, "out": "H", "offset": [0]},
+    {"page": 0, "key": "sample depth (ft):", "num": 3, "out": "H", "offset": [0]},
+    {"page": 0, "key": "sample depth:", "num": 3, "out": "H", "offset": [0]},
+    {"page": 0, "key": "liquid limit:", "num": 1, "out": "I", "offset": [0]},
+    {"page": 0, "key": "plastic limit:", "num": 1, "out": "J", "offset": [0]},
+    {"page": 0, "key": "plastic index:", "num": 1, "out": "K", "offset": [0]},
+    {"page": 0, "key": "initial water content, (%)", "num": 3, "out": "L", "offset": [0, 1, 2]},
+    {"page": 0, "key": "dry unit weight (kn/m3)", "num": 3, "out": "T", "offset": [0, 1, 2]},
+    {"page": 0, "key": "void ratio", "num": 3, "out": "U", "offset": [0, 1, 2]},
+    # {"page": 0, "key": "", "num": 1, "out": "", "offset": [0]},
+]
 
-    def __del__(self):
-        if (self.doc != None):
-            self.doc.close()
 
-    def num_pages(self) -> int:
-        return self.doc.page_count
+def extract_file(source: str, destination: str):
+    log.info("Extracting data from file `%s` to `%s` ...", source, destination)
 
-    def read_page(self, page_num: int) -> str:
-        page = self.doc[page_num]
-        return page.get_text(sort=True)
+    with fitz.open(source) as doc:
+        wb_out = xcl.open_workbook(destination)
+        _extract_one(doc, wb_out)
 
-    def find_values(self, page_num: int, key: str, num_vals = 1) -> list:
-        """Finds words on a page that are preceded by a parsed 'key' words."""
-        key_words = key.split()
-        page = self.doc[page_num]
-        page_boxes = page.get_text("words", sort=True)
-        # As per https://pymupdf.readthedocs.io/en/latest/textpage.html#TextPage.extractWORDS
-        page_words = [row[4] for row in page_boxes]
 
-        num_keys = len(key_words)
-        num_words = len(page_words)
+def extract_dir(source: str, destination: str):
+    log.info("Extracting data from directory '%s' to '%s' ...", source, destination)
 
-        for i in range(num_words - num_keys + 1):
-            if page_words[i : i + num_keys] == key_words:
-                return page_words[i + num_keys : i + num_keys + num_vals]
+    pdf_files = _list_pdf_files(source)
+    if len(pdf_files) == 0:
+        log.warning("No PDF files found in '%s'", source)
+        return
 
-        return None
+    wb_out = xcl.open_workbook(destination)
+    start_row = xcl.max_row(wb_out) + 1
+    for source in pdf_files:
+        with fitz.open(source) as doc:
+            _extract_one(doc, wb_out)
+
+    end_row = xcl.max_row(wb_out)
+    xcl.sort_rows(wb_out, start_row, end_row)
+
+    xcl.save_workbook(wb_out, destination)
+    log.info("Done directory extracting")
+
+def _list_pdf_files(dir_path: str):
+    pdf_files = []
+    for fn in os.listdir(dir_path):
+        full_path = os.path.join(dir_path, fn)
+        if full_path.lower().endswith(".pdf") and os.path.isfile(full_path):
+            pdf_files.append(full_path)
+
+    return pdf_files
+
+def _extract_one(doc: fitz.Document, wb_out: xcl.ExcelWorkbook):
+    last_row = xcl.max_row(wb_out)
+    log.info("Extracting data from '%s' from row %d ...", doc.name, last_row)
+
+    for mapping in MAPPINGS:
+        _copy_one_value(doc, wb_out, last_row, mapping)
+
+    for col in xcl.MERGE_COLS:
+        xcl.merge_cells(wb_out, col, last_row)
+
+
+def _copy_one_value(doc: fitz.Document, wb_out: xcl.ExcelWorkbook, last_row: int, mapping: Any):
+    page_num = mapping["page"]
+    key = mapping["key"]
+    num_vals =  mapping["num"]
+    column = mapping["out"]
+    offset = mapping["offset"]
+
+    key_words = key.lower().split()
+    page = doc[page_num]
+    page_boxes = page.get_text("words", sort=True)
+
+    # As per https://pymupdf.readthedocs.io/en/latest/textpage.html#TextPage.extractWORDS
+    page_words = [row[4] for row in page_boxes]
+
+    num_keys = len(key_words)
+    num_words = len(page_words)
+
+    for i in range(num_words - num_keys + 1):
+        if [s.lower() for s in page_words[i : i + num_keys]] == key_words:
+            values = page_words[i + num_keys : i + num_keys + num_vals]
+            if num_vals > 1 and len(offset) == 1:
+                xcl.insert_one_value(_merge_vals(values), wb_out, last_row + 1, column)
+            else:
+                for j, val in enumerate(values):
+                    xcl.insert_one_value(val, wb_out, last_row + offset[j] + 1, column)
+
+            break
+
+
+# Major cases:
+# 1. three values are strings with '/' separator
+# 2. thee values are numbers with optional units separated with '-'
+# 3. first value is number with optional unit
+def _merge_vals(values: list) -> Any:
+    if len(values) == 3:
+        if values[1] == '/':
+            return values[0] + '_' + values[2]
+        else:
+            val1 = cnv.convert_value(cnv.remove_units(values[0]))
+            val2 = cnv.convert_value(cnv.remove_units(values[2]))
+            if values[1] == '-' and not math.isnan(val1) and not math.isnan(val2):
+                return (val1 + val2) / 2
+            elif not math.isnan(val1):
+                return val1
+
+    return "".join(values)
--- tests/test_pdf_extractor.py
+++ tests/test_pdf_extractor.py
@@ -1,45 +1,42 @@
 import unittest
-import os
-# from unittest.mock import Mock, patch
+from unittest.mock import Mock, patch
 import fitz # PyMuPDF
-from openpyxl import load_workbook, Workbook
-# import converter as cnv
 import pdf_extractor as pdf
+import xcl_extractor as xcl
 
 
-class TestExtractor(unittest.TestCase):
+def identity_fun(arg):
+    return arg
+
+
+# pylint: disable=protected-access
+class TestPdfExtractor(unittest.TestCase):
 
     def setUp(self):
-        # Create dummy Excel files for testing
-        self.source_file = "test_source.pdf"
-        self.destination_file = "test_destination.xlsx"
+        self.doc = fitz.open()
+        page = self.doc.new_page(width=595, height=842)
+        page.insert_text((50, 50), "Client:")
+        page.insert_text((110, 50), "Client Name")
+        page.insert_text((200, 50), "Borehole/Sample No.:")
+        page.insert_text((350, 50), "ENG-PQ24-15 / SC40")
+        page.insert_text((200, 70), "Sample Depth (m):")
+        page.insert_text((350, 70), "56.9 - 57.6m")
+        page.insert_text((200, 90), "Liquid Limit:")
+        page.insert_text((350, 90), "39")
 
-
-        with fitz.open() as doc:
-            page = doc.new_page(width=595, height=842)
-            page.insert_text((50, 50), "Client:")
-            page.insert_text((110, 50), "Client Name")
-            page.insert_text((200, 50), "Borehole/Sample No.:")
-            page.insert_text((350, 50), "ENG-PQ24-15 / SC40")
-            page.insert_text((200, 70), "Sample Depth (m):")
-            page.insert_text((350, 70), "56.9 - 57.6m")
-            page.insert_text((200, 90), "Liquid Limit:")
-            page.insert_text((350, 90), "39")
-            doc.save(self.source_file)
-
-        wb_destination = Workbook()
-        ws_destination = wb_destination.active
-        ws_destination.title = "Summary by Type"
-        wb_destination.save(self.destination_file)
+        # wb_destination = Workbook()
+        # ws_destination = wb_destination.active
+        # ws_destination.title = "Summary by Type"
+        # wb_destination.save(self.destination_file) #UC ???
 
 
     def tearDown(self):
+        self.doc.close()
         # Clean up test files
         # if os.path.exists(self.source_file):
         #     os.remove(self.source_file)
         # if os.path.exists(self.destination_file):
         #     os.remove(self.destination_file)
-        pass
 
 
     def test_validate_mappings(self):
@@ -50,7 +47,58 @@ class TestExtractor(unittest.TestCase):
             self.assertGreaterEqual(mapping["num"], len(mapping["offset"]))
 
 
-    def test_extract_file(self):
-        pdf.extract_file(self.source_file, self.destination_file)
+    def test_merge_vals(self):
+        self.assertEqual("ENG-PQ24-15_SC40", pdf._merge_vals(["ENG-PQ24-15", "/", "SC40"]))
+        self.assertEqual(3.0, pdf._merge_vals(["2", "-", "4"]))
+        self.assertEqual(3.0, pdf._merge_vals(["2", "-", "4m"]))
+        self.assertEqual(2.0, pdf._merge_vals(["2", "something", "4"]))
+        self.assertEqual("ab", pdf._merge_vals(["a", "b"]))
 
-        #UC wb_in = load_workbook(self.source_file, data_only=True)
+
+    def test_copy_one_value_basic(self):
+        wb_out = Mock()
+        mock_output_sheet = Mock()
+        wb_out.active = mock_output_sheet
+        excl_wb = xcl.ExcelWorkbook(wb_out)
+
+        # Test mapping
+        mapping = { "page": 0, "key": "client:", "num": 1, "out": "B", "offset": [0]}
+
+        with patch('xcl_extractor.cnv.convert_na', side_effect=identity_fun), \
+             patch('xcl_extractor.cnv.col_name_to_idx', return_value=2):
+
+            pdf._copy_one_value(self.doc, excl_wb, 10, mapping)
+
+        mock_output_sheet.cell.assert_called_once_with(
+            row=11,  # last_row + offset + 1 = 10 + 0 + 1
+            column=2,  # column B
+            value="Client" #UC "Client Name"!
+        )
+
+
+    def test_copy_one_value_with_merge(self):
+        wb_out = Mock()
+        mock_output_sheet = Mock()
+        wb_out.active = mock_output_sheet
+        excl_wb = xcl.ExcelWorkbook(wb_out)
+
+        # Test mapping
+        mapping = { "page": 0, "key": "borehole/sample no.:", "num": 3, "out": "B", "offset": [0]}
+
+        # Mock converter functions
+        with patch('xcl_extractor.cnv.convert_na', side_effect=identity_fun), \
+             patch('xcl_extractor.cnv.col_name_to_idx', return_value=2):
+
+            pdf._copy_one_value(self.doc, excl_wb, 10, mapping)
+
+        mock_output_sheet.cell.assert_called_once_with(
+            row=11,  # last_row + offset + 1 = 10 + 0 + 1
+            column=2,  # column B
+            value="ENG-PQ24-15_SC40"
+        )
+
+
+    #UC def test_extract_file(self): #UC just for testing
+    #     pdf.extract_file(self.source_file, self.destination_file)
+
+    #     wb_in = load_workbook(self.source_file, data_only=True)
